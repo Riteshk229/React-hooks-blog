a99ceb548c5bb8bccc9b60385369d310a464de6e
Ritesh <Riteshk229@gmail.com>
2023-07-01 20:38:32 +0530

    third party styled component

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,18 +1,27 @@
 import { Link } from "react-router-dom";
 import styled from 'styled-components';
 
+// to style third party components pass it as a =n argument in styled
+const StyledLink = styled(Link)`
 
-const StyledLink = styled(Link)``;
+    text-decoration: none;
+    color: #9e9e9e;
+
+    &:hover{
+        text-decoration: underline;
+      }
+
+`;
 
 function Navbar(){
     return (
         <div >
             <ul id="nav">
                 <li>
-                    <Link to='/'>Home</Link>
+                    <StyledLink to='/'>Home</StyledLink>
                 </li>
                 <li>
-                    <Link to='/post/create-post'>Create Post</Link>
+                    <StyledLink to='/post/create-post'>Create Post</StyledLink>
                 </li>
             </ul>
         </div>

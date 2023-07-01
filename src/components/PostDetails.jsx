b00415b996a3af34cf31af2a0d97019c750272d9
Ritesh <Riteshk229@gmail.com>
2023-07-01 13:16:37 +0530

    added postdetails to show posts

--- src/components/PostDetails.jsx
+++ src/components/PostDetails.jsx
@@ -1,7 +1,30 @@
+import { useEffect, useState } from "react";
+import { firestore } from "../firebase";
+import { useParams } from "react-router";
+
 function PostDetails(){
+    const [post,setPost] =useState('');
+    const {postID} = useParams();
+
+    useEffect(()=>{
+        firestore
+            .collection("posts")
+            .doc(postID)
+            .get()
+            .then((snapshot)=>{
+                console.log("SnapShot :",snapshot.data());
+                const post = snapshot.data();
+
+                console.log(post);
+
+                setPost(post);
+            });
+    },[]);
+
     return (
-        <div className="">
-            PostDetails
+        <div className="post-details">
+            <h1>{post.title}</h1>
+            <p>{post.content}</p>
         </div>
     )
 }
